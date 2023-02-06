1e0103c3f0aff8f551eec0921ebdc4664210cd6b
MapComplete <pietervdvn@posteo.net>
2023-02-06 00:38:08 +0000

    Deploying a new version

--- assets/BackToIndex-78953dd7.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./SubtleButton-259bcabf.js";import{S as a,T as o}from"./Translations-506890bc.js";class n extends e{constructor(t){super(a.back_svg().SetStyle("height: 1.5rem;"),t??o.t.general.backToMapcomplete,{url:"index.html"})}}export{n as B};
