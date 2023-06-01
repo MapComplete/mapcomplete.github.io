5f79cb3669f10b59c92af5b6aa7e286b01005285
MapComplete <pietervdvn@posteo.net>
2023-06-01 09:37:09 +0000

    Deploying a new version

--- service-worker.js
+++ service-worker.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var version = "0.0.8-40fcc9e225abc1c862121e65aa0f09444b19048d";
+var version = "0.0.8-5d19d563db5622430d6dbf9b87b5fe7b8eda42a2";
 function install() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
