3b5efec9260f0344bb50987f935b4a0520eb7330
pietervdvn <pietervdvn@posteo.net>
2024-06-27 01:59:48 +0000

    Deploying a new version of mapcomplete

--- service-worker.js
+++ service-worker.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var version = "0.0.8-de9691e72392e97c4c39a6430e066417595e1799";
+var version = "0.0.8-a281381284d8d5043c0275c05f95ecaefc955333";
 function install() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
