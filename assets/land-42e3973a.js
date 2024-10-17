a5cb1a9b6e4f47502d4c4ad51c07164c09561213
pietervdvn <pietervdvn@posteo.net>
2024-10-17 18:17:34 +0000

    Deploying a new version of mapcomplete

---
+++ assets/land-42e3973a.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-3cfb730f.js";import{O as i}from"./OsmConnection-fa95ab3c.js";import"./UIEventSource-64bbdd19.js";import"./LocalStorageSource-c3dcf760.js";import"./Constants-823bf632.js";console.log("Authorizing...");new i().finishLogin(o=>{const n=window.location.protocol+"//"+window.location.host+"/index.html";o??(o=n),o.indexOf("/land")>0&&(o=n),console.log("Redirecting to",o),window.location.href=o});
+//# sourceMappingURL=land-42e3973a.js.map
